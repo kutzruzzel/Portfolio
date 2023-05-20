b65d0ccfc32a10733e1080730e083f235702a9cf
Kutz <kutzruzzel.sumaya60@gmail.com>
2023-05-20 14:44:23 +0800

    design improvements

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
       <Navbar/>
         <div className='intro'>
        <p className='p1'>Welcome to my portfolio!</p>
-       <p className='p2'>I am Kutz Ruzzel Sumaya, a web developer with a strong focus on creating engaging and dynamic user experiences.</p>
+       <p className='p2'>I am Kutz Ruzzel Sumaya, a Full Stack Developer with a strong focus on creating engaging and dynamic user experiences.</p>
        </div>
        
        <div className='footer'>
-       <p className='p3'>Call me: 123-456-789</p>
+       
        <p className='p4'>Email: kutzsumaya@yahoo.com </p>
        <Link to="https://github.com/Kutz-S" >
        <div>
